7cc6bc73168065a575c7ba73ee926d4415dc8d09
akashsateesh <akashsateesh.417@gmail.com>
2016-09-28 22:41:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
