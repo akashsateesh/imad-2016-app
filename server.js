0b7782bc33016e83215a06e8ddad53bee5ea438d
akashsateesh <akashsateesh.417@gmail.com>
2016-10-08 20:53:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,43 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title :'Article-one',
+    heading :'Article-one',
+    date : 'sept 5',
+    content : `
+        <div Class="container">
+            <hr/>
+            <ol>
+                <li>Hello
+                </li>
+                <li>Bye
+                </li>
+            </ol><br>
+            <p>hello this is gazillian cover.Hope you enjoyed this and stay happy bye</p><br/>
+            <a href="http://www.w3schools.com">Visit W3Schools</a>
+        </div>`
+};
+function create(data){
+    var title=data.title;
+    var content=data.content;
+var html=`<html>
+    <body>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <title>Article two</title>
+        <h1>Hi displaying article two</h1>
+         ${content}
+    </body>
+</html>`;
+return html;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(create(articleone));
 });
 
 
