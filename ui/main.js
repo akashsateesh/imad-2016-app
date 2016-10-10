5ec3a85e38854fca5142ec5a2a4c124c8bad99d5
akashsateesh <akashsateesh.417@gmail.com>
2016-10-10 16:37:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 var e=document.getElementById("ok");
 e.innerHTML="yes got u";
-var img=document.getElementById("madi");
-img.onclick=function(){
-    img.style.marginLeft= '100px';
-     img.style.height= '100px';
+var madi=document.getElementById("madi");
+madi.onclick=function(){
+    madi.style.marginLeft= '100px';
+     //img.style.height= '100px';
 };
