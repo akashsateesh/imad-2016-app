86945f407e10cba9ce66d2535d0c1d30d012b596
akashsateesh <akashsateesh.417@gmail.com>
2016-10-10 16:17:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ app.get('/', function (req, res) {
 app.get('/article-two', function (req, res) {
   res.send(create(articleone));
 });
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
