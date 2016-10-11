b56707aeeb2475fa60b762fb291b596d6b7769a7
akashsateesh <akashsateesh.417@gmail.com>
2016-10-11 22:47:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,18 @@ console.log('Loaded!');
 var c=0;
 var e=document.getElementById("counter");
 e.onclick=function(){
-    var s=document.getElementById("count");
-    s.innerHTML=++c;
+    var request=new XMLHttpRequest();
+    //var s=document.getElementById("count");
+    request.onreadystatechange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200)
+            {
+                c=request.responseText;
+                 var s=document.getElementById("count");
+                 s.innerHTML=++c;
+            }
+        }
+    };
+    request.open('GET','http://akashsateesh.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
