78911f2998ec94dee735fcf686b9c99fe6a05283
akashsateesh <akashsateesh.417@gmail.com>
2016-11-20 12:07:55 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,41 @@
 console.log('Loaded!');
-var c=0;
-var e=document.getElementById("counter");
-e.onclick=function(){
+function show(){
+    var user=document.getElementById("inputuser");
+    var pass=document.getElementById("inputPassword");
+    var arry=[user.value,pass.value];
+    console.log('yes its logged');
     var request=new XMLHttpRequest();
-    //var s=document.getElementById("count");
+   
     request.onreadystatechange=function(){
         if(request.readyState==XMLHttpRequest.DONE){
-            if(request.status==200)
+           if(request.status==200)
             {
-                c=request.responseText;
-                 var s=document.getElementById("count");
-                 s.innerHTML=++c;
+                 c=request.responseText;
+                 if(c==null)alert("Validation unsuccessful");
+                 else
+                 window.open("http://www.rvce.edu.in");
+                 
             }
-        }
-    };
-    request.open('GET','http://akashsateesh.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
+        }  
+}
+request.open('POST','http://localhost:8080/login',true);
+request.send(arry);
+}
+document.getElementById("how").onclick=function(){
+    window.location.href = "http://localhost:8080/ui/how"; 
+}
+document.getElementById("farmer").onclick=function(){
+     window.location.href = "http://localhost:8080/ui/hello.html";
+}
+document.getElementById("dbms").onclick=function(){
+    window.location.href = "http://localhost:8080/ui/hello.html";
+}
+document.getElementById('trader').onclick=function(){
+     window.location.href = "http://localhost:8080/ui/hello.html";
+}
+document.getElementById('about').onclick=function(){
+  window.location.href = "http://localhost:8080/";
+}
+document.getElementById('register').onclick=function(){
+  window.location.href = "http://localhost:8080/register";
+}
